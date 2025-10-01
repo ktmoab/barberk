272f03dad8dea3ca52b59df9a45017f98545d1d4
motijirata <jirata52@gmail.com>
2025-10-01 16:11:51 +0200

    hero image updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,14 +89,14 @@ export default function Home() {
       <div className="h-[72px] md:h-[100px]" aria-hidden />
 
       {/* HERO */}
-      <section className="relative w-full min-h-[65vh] flex items-center justify-center">
+      <section className="relative w-full h-[31vh] sm:h-[40vh] md:h-[60vh] min-h-[190px] max-h-[400px] md:max-h-[700px] flex items-center justify-center">
         <div className="absolute inset-0">
           {/* Use fill + container so we can control object-fit per breakpoint */}
           <Image
             src="/images/thebarberk-hero.jpg"
             alt="Hero"
             fill
-            className="object-top md:object-center"
+            className="object-contain "
             priority
           />
           <div className="absolute inset-0 bg-black/40" />
